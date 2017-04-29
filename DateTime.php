36a503d8e016f3222c76d14772e1df27d499664a
atishamte <atishamte@gmail.com>
2017-04-29 17:47:54 +0530

    Committing final functionality

--- DateTime.php
+++ DateTime.php
@@ -7,31 +7,70 @@ namespace atishamte;
  *
  * @package atishamte
  */
-class DateTime
+abstract class DateTime
 {
+	static $_formats = [
+		'DT' => [
+			'Y-m-d H:i:s',
+			'd-m-Y H:i:s',
+			'm/d/Y H:i:s',
+			'Y-m-d g:i A',
+			'd-m-Y g:i A',
+			'm/d/Y g:i A',
+			'Y-m-d h:i A',
+			'd-m-Y h:i A',
+			'm/d/Y h:i A',
+		],
+		'D'  => [
+			'Y-m-d',
+			'd-m-Y',
+			'm/d/Y',
+		],
+		'T'  => [
+			'H:i:s',
+			'H:i a',
+			'h:i a',
+		],
+	];
+
 	/**
-	 * @param string $start_date Expected Format: 21/12/2016 | 2016-12-21 | 21-12-2016
-	 * @param string $end_date   Expected Format: 21/12/2016 | 2016-12-21 | 21-12-2016
+	 * @param string $start_date Expected Format: 12/21/2016 | 2016-12-21 | 21-12-2016
+	 * @param string $end_date   Expected Format: 12/21/2016 | 2016-12-21 | 21-12-2016
 	 * @param string $unit       Example Unit: day | month | year
 	 *
 	 * @return string Date difference in passed unit in argument
 	 */
-	public static function dateDiff($start_date, $end_date = 'NOW', $unit = 'day')
+	public static function dateDiff($start_date, $end_date = '', $unit = 'day')
 	{
-		return '';
+		if (self::validateDateTime($start_date, 'D') && self::validateDateTime($end_date, 'D')) {
+			if (in_array($unit, ['day', 'month', 'year'])) {
+				return self::diff($start_date, $end_date, $unit);
+			} else {
+				die('Invalid unit passed');
+			}
+		}
+
+		return false;
 	}
 
 	/**
-	 * @param string $start_time Expected Format: 08:29 PM | 8:29 PM | 20:29
-	 * @param string $end_time   Expected Format: 08:29 PM | 8:29 PM | 20:29
-	 * @param string $time_type  Expected Format: 12(12 Hours) | 24(24Hours)
+	 * @param string $start_time Expected Format: 04:29 AM | 4:29 AM | 04:29
+	 * @param string $end_time   Expected Format: 04:29 AM | 4:29 AM | 04:29
 	 * @param string $unit       Expected Format: second | minute | hour
 	 *
 	 * @return string Time difference in passed unit in argument
 	 */
-	public static function timeDiff($start_time, $end_time = 'NOW', $time_type = '24', $unit = 'second')
+	public static function timeDiff($start_time, $end_time = '', $unit = 'second')
 	{
-		return '';
+		if (self::validateDateTime($start_time, 'T') && self::validateDateTime($end_time, 'T')) {
+			if (in_array($unit, ['second', 'minute', 'hour'])) {
+				return self::diff($start_time, $end_time, $unit);
+			} else {
+				die('Invalid unit passed');
+			}
+		}
+
+		return false;
 	}
 
 	/**
@@ -40,13 +79,21 @@ class DateTime
 	 * @param string $unit Expected Format: second | minute | hour | day | month | year
 	 *
 	 * $start_datetime, $end_datetime expected formats are,
-	 * combination of date like 21/12/2016 | 2016-12-21 | 21-12-2016 and time like 08:29 PM | 8:29 PM | 20:29
+	 * combination of date like 12/21/2016 | 2016-12-21 | 21-12-2016 and time like 04:29 AM | 4:29 AM | 04:29
 	 *
 	 * @return string Date-Time difference in passed unit in argument
 	 */
-	public static function dateTimeDiff($start_datetime, $end_datetime = 'NOW', $unit = 'second')
+	public static function dateTimeDiff($start_datetime, $end_datetime = '', $unit = 'second')
 	{
-		return '';
+		if (self::validateDateTime($start_datetime, 'DT') && self::validateDateTime($end_datetime, 'DT')) {
+			if (in_array($unit, ['day', 'month', 'year', 'second', 'minute', 'hour'])) {
+				return self::diff($start_datetime, $end_datetime, $unit);
+			} else {
+				die('Invalid unit passed');
+			}
+		}
+
+		return false;
 	}
 
 	/**
@@ -56,7 +103,11 @@ class DateTime
 	 */
 	public static function currentDate($format = 'Y-m-d')
 	{
-		return date($format, time());
+		if (self::validateFormat($format, 'D')) {
+			return self::getCurrent($format);
+		}
+
+		return false;
 	}
 
 	/**
@@ -66,7 +117,11 @@ class DateTime
 	 */
 	public static function currentTime($format = 'H:i:s')
 	{
-		return date($format, time());
+		if (self::validateFormat($format, 'T')) {
+			return self::getCurrent($format);
+		}
+
+		return false;
 	}
 
 	/**
@@ -76,75 +131,137 @@ class DateTime
 	 */
 	public static function currentDateTime($format = 'Y-m-d H:i:s')
 	{
-		return date($format, time());
-	}
+		if (self::validateFormat($format, 'DT')) {
+			return self::getCurrent($format);
+		}
 
-    /**
-     * @param string $datetime Expected Format: 21/12/2016 | 2016-12-21 | 21-12-2016
-     * @param string $type  DT:DateTime | D:Date | T:Time
-     * @param string $format Desired Date or Time format.
-     *                       Default Formats : Date -> Y-m-d,
-     *                                         Time -> H:i:s
-     *
-     * @return bool|string
-     */
-    public static function formatDateTime($datetime, $type = 'DT', $format = '')
-	{
-		$datetime = str_replace('/', '-', $datetime);
-		$type = strtoupper($type);
-		if ($type == 'DT') {
-			return date('jS-M-Y g:iA', strtotime($datetime));
-		}
-		if ($type == 'D') {
-			return date('jS-M-Y', strtotime($datetime));
-		}
-		if ($type == 'T') {
-			return date('g:iA', strtotime($datetime));
-		}
 		return false;
 	}
 
 	/**
-	 * @param string $return Expected Format : year | month | day | hour | minute | second
-	 * @param string $date1  Expected Format: 21/12/2016 | 2016-12-21 | 21-12-2016
-	 * @param string $date2  Optional Field(If not passed, current datetime will be considered)
-     *                       If Passed, Expected Format: 21/12/2016 | 2016-12-21 | 21-12-2016
+	 * @param string $datetime                 Expected Format: 12/21/2016 | 2016-12-21 | 21-12-2016
+	 * @param string $type                     DT:DateTime | D:Date | T:Time
+	 * @param string $format                   Desired Date or Time format.
+	 *                                         Default Formats : Date -> Y-m-d,
+	 *                                         Time -> H:i:s
 	 *
-	 * @return mixed
+	 * @return bool|string
 	 */
-	public static function get_date_diff($return, $date1, $date2 = '')
+	public static function formatDateTime($datetime, $type = 'DT', $format = '')
 	{
-		$datetime1 = new \DateTime($date1);
-		$datetime2 = new \DateTime($date2);
+		if (!self::validateDateTime($format, $type)) {
+			switch (strtoupper($type)) {
+				case 'DT':
+					$format = 'Y-m-d H:i:s';
+					break;
+				case 'D':
+					$format = 'Y-m-d';
+					break;
+				case 'T':
+					$format = 'H:i:s';
+					break;
+			}
+		}
 
+		return date($format, strtotime($datetime));
+	}
+
+	/**
+	 * @param string $distant_timestamp            expected formats are, combination of date like 12/21/2016 | 2016-12-21 | 21-12-2016 and time like 04:29 AM | 4:29 AM | 04:29
+	 * @param int    $max_units                    Expected Input : 1 -> year,
+	 *                                             2 -> month,
+	 *                                             3 -> day,
+	 *                                             4 -> hour,
+	 *                                             5 -> minute,
+	 *                                             6 -> second
+	 *
+	 * @return string
+	 */
+	public static function getTimeAgo($distant_timestamp, $max_units = 6)
+	{
+		if (self::validateDateTime($distant_timestamp)) {
+			$date = new \DateTime();
+			$date->setTimestamp(strtotime($distant_timestamp));
+			$date = $date->diff(new \DateTime());
+			// build array
+			$since = json_decode($date->format('{"year":%y,"month":%m,"day":%d,"hour":%h,"minute":%i,"second":%s}'), true);
+			// output only the first x date values
+			$since = array_slice($since, 0, $max_units);
+			// build string
+			$last_key = key(array_slice($since, -1, 1, true));
+			$string = '';
+			foreach ($since as $key => $val) {
+				// separator
+				if ($string) {
+					$string .= $key != $last_key ? ', ' : ' ' . 'and' . ' ';
+				}
+				// set plural
+				$key .= $val > 1 ? 's' : '';
+				// add date value
+				$string .= $val . ' ' . $key;
+			}
+
+			return $string;
+		}
+
+		return false;
+	}
+
+	private function validateDateTime($formatted_datetime, $type = 'DT')
+	{
+		$validate = false;
+		foreach (self::$_formats[$type] as $f) {
+			if (!$validate) {
+				$d = \DateTime::createFromFormat($f, $formatted_datetime);
+				$validate = $d && $d->format($f) == $formatted_datetime;
+			}
+		}
+
+		return $validate;
+	}
+
+	private function validateFormat($format, $type)
+	{
+		$validate = false;
+		foreach (self::$_formats[$type] as $f) {
+			if (!$validate) {
+				$validate = $f == $format;
+			}
+		}
+
+		return $validate;
+	}
+
+	private function getCurrent($format)
+	{
+		return date($format, time());
+	}
+
+	private function diff($start_datetime, $end_datetime, $unit)
+	{
+		$datetime1 = new \DateTime($start_datetime);
+		$datetime2 = new \DateTime($end_datetime);
 		$difference = $datetime1->diff($datetime2);
-
-		$diff = FALSE;
-
-		switch (strtolower($return)) {
+		$diff = false;
+		switch (strtolower($unit)) {
 			case 'year':
 				$diff = $difference->y;
 				break;
-
 			case 'month':
 				$diff = ($difference->y * 12) + $difference->m;
 				break;
-
 			case 'day':
 				$diff = $difference->days;
 				break;
-
-            case 'hour':
-                $diff = ($difference->days * 24) + $difference->h;
-                break;
-
-            case 'minute':
-                $diff = ((($difference->days * 24) + $difference->h) * 60) + $difference->i;
-                break;
-
-            case 'second':
-                $diff = ((((($difference->days * 24) + $difference->h) * 60) + $difference->i) * 60) + $difference->s;
-                break;
+			case 'hour':
+				$diff = ($difference->days * 24) + $difference->h;
+				break;
+			case 'minute':
+				$diff = ((($difference->days * 24) + $difference->h) * 60) + $difference->i;
+				break;
+			case 'second':
+				$diff = ((((($difference->days * 24) + $difference->h) * 60) + $difference->i) * 60) + $difference->s;
+				break;
 		}
 		if ($datetime1 > $datetime2) {
 			$diff = $diff * (-1);
@@ -152,57 +269,4 @@ class DateTime
 
 		return $diff;
 	}
-
-    /**
-     * @param string $distant_timestamp            expected formats are, combination of date like 21/12/2016 | 2016-12-21 | 21-12-2016 and time like 08:29 PM | 8:29 PM | 20:29
-     * @param int    $max_units                    Expected Input : 1 -> year,
-     *                                             2 -> month,
-     *                                             3 -> day,
-     *                                             4 -> hour,
-     *                                             5 -> minute,
-     *                                             6 -> second
-     *
-     * @return string
-     */
-	public static function get_time_ago($distant_timestamp, $max_units = 6)
-	{
-        $date = new \DateTime();
-        $date->setTimestamp(strtotime($distant_timestamp));
-        $date = $date->diff(new \DateTime());
-
-        // build array
-        $since = json_decode($date->format('{"year":%y,"month":%m,"day":%d,"hour":%h,"minute":%i,"second":%s}'), true);
-
-        // output only the first x date values
-        $since = array_slice($since, 0, $max_units);
-
-        // build string
-        $last_key = key(array_slice($since, -1, 1, true));
-
-        $string = '';
-
-        foreach ($since as $key => $val) {
-            // separator
-            if ($string) {
-                $string .= $key != $last_key ? ', ' : ' ' . 'and' . ' ';
-            }
-            // set plural
-            $key .= $val > 1 ? 's' : '';
-            // add date value
-            $string .= $val . ' ' . $key;
-        }
-
-        return $string;
-	}
-
-    /**
-     * @param $formatted_datetime   DateTime string
-     *
-     * @return bool
-     */
-    private function validateFormat($formatted_datetime)
-    {
-        return false;
-
-    }
 }
